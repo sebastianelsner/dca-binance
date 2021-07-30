e9d969065d6df232f1316e05a06c55f2503887c6
Sebastian Elsner <sebastian@risefx.com>
2021-07-30 09:26:26 +0200

    pure formatting

--- dca_binance/logger.py
+++ dca_binance/logger.py
@@ -5,8 +5,11 @@ __author__ = "Louis Aussedat"
 __copyright__ = "Copyright (c) 2021 Louis Aussedat"
 __license__ = "GPLv3"
 
+
 def setup_logger(name, level):
-    logFormatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-6s %(message)s")
+    logFormatter = logging.Formatter(
+        "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    )
     rootLogger = logging.getLogger()
 
     consoleHandler = logging.StreamHandler()
@@ -17,11 +20,15 @@ def setup_logger(name, level):
 
     return log
 
+
 def setup_logger_filename(name, path):
     rootLogger = logging.getLogger()
-    logFormatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-6s %(message)s")
+    logFormatter = logging.Formatter(
+        "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    )
 
     fileHandler = logging.handlers.TimedRotatingFileHandler(
-        "{0}/{1}.log".format(path, "dca-binance"), when='D', interval=31)
+        "{}/{}.log".format(path, "dca-binance"), when="D", interval=31
+    )
     fileHandler.setFormatter(logFormatter)
     rootLogger.addHandler(fileHandler)
